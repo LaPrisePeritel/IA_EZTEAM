e6f94ef9bcdebc5aa4b2c4e9adf627bc61e7e67d
Str0ke_ <charlelie.garrigues@gmail.com>
2022-11-03 11:03:11 +0100

    WIP follow

--- Assets/=Enterprise=/ShipController.cs
+++ Assets/=Enterprise=/ShipController.cs
@@ -10,18 +10,24 @@ namespace EnterpriseTeam {
     {
         private Vector2 targetPoint;
 
+        public float rotation { get; set; }
+        public uint thrust { get; set; }
+
+
+        public SpaceShipView view { get; private set; }
+        public GameData Data { get; private set; }
+
 		public override void Initialize(SpaceShipView spaceship, GameData data)
-		{
-		}
+        {
+            view = spaceship;
+            Data = data;
+        }
 
 		public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
 		{
-            Vector2 v_diff = (targetPoint - (Vector2)transform.position);
-            float atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
-
             SpaceShipView otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
 			float thrust = 1.0f;
-			float targetOrient = Quaternion.Euler(0f, 0f, atan2 * Mathf.Rad2Deg).z;
+            float targetOrient = rotation;
 			bool needShoot = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
 			return new InputData(thrust, targetOrient, needShoot, false, false);
 		}
